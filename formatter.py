43d1c4d35ec153d3f23b003858b15aea1bb7e7fd
F70st <damir.serazetdinow@yandex.ru>
2025-09-03 23:02:00 +0300

    Замена nbsp на пробелы

--- formatter.py
+++ formatter.py
@@ -7,8 +7,10 @@ for subdir, dirs, files in os.walk(rootdir):
     if regex.search(r'\p{IsCyrillic}', subdir):
         for file_name in files:
             with open(os.path.join(subdir, file_name), encoding='UTF8', mode='r+') as file:
-                print(file_name)
                 text = file.read()
-                text = text.replace(u'\xa0', ' ')
+                if u'\xa0' in text:
+                    print(file_name)
+                    text = text.replace(u'\xa0', ' ')
 
-                file.write(text)
+                    file.seek(0)
+                    file.write(text)
