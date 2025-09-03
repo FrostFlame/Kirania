22c7577fae15dc8fa7cb18c9e4417940add8b046
F70st <damir.serazetdinow@yandex.ru>
2025-09-03 23:57:29 +0300

    Полные линки на всё + скрипт

--- formatter.py
+++ formatter.py
@@ -11,6 +11,19 @@ for subdir, dirs, files in os.walk(rootdir):
                 if u'\xa0' in text:
                     print(file_name)
                     text = text.replace(u'\xa0', ' ')
-
                     file.seek(0)
                     file.write(text)
+
+                bare_links = regex.findall(r'\[\[#.*?\]\]', text)
+                if bare_links:
+                    for bare_link in bare_links:
+                        bare_link = bare_link[2:-2]
+                        link = '[[' + file_name[:-3] + bare_link
+                        if '|' not in link:
+                            link += '\|' + bare_link[1:]
+                        link += ']]'
+                        text = text.replace(f'[[{bare_link}]]', link)
+                    file.seek(0)
+                    file.write(text)
+
+
