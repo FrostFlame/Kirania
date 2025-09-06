2f1f86037061deabffce4ddc53d368ca9e79ed0f
F70st <damir.serazetdinow@yandex.ru>
2025-09-06 13:58:38 +0300

    Исправление таблиц

--- formatter.py
+++ formatter.py
@@ -7,16 +7,12 @@ for subdir, dirs, files in os.walk(rootdir):
     if regex.search(r'\p{IsCyrillic}', subdir):
         for file_name in files:
             with open(os.path.join(subdir, file_name), encoding='UTF8', mode='r+') as file:
+                changed = False
+                print(file_name)
                 text = file.read()
                 if u'\xa0' in text:
                     text = text.replace(u'\xa0', ' ')
-                    file.seek(0)
-                    file.write(text)
-
-                if '\|' in text:
-                    text = text.replace('\|', '|')
-                    file.seek(0)
-                    file.write(text)
+                    changed = True
 
                 bare_links = regex.findall(r'\[\[#.*?\]\]', text)
                 if bare_links:
@@ -27,7 +23,18 @@ for subdir, dirs, files in os.walk(rootdir):
                             link += '|' + bare_link[1:]
                         link += ']]'
                         text = text.replace(f'[[{bare_link}]]', link)
+                    changed = True
+
+                if 'Стрелок' in file_name or 'Следопыт' in file_name:
+                    pass
+                broken_links_in_tables = regex.findall(r'\|\s[^|]*?\[\[[^\]]+?\S\|\S.+?\]\]\s*?\|', text)
+                for broken_link in broken_links_in_tables:
+                    if '\\|' in broken_link:
+                        continue
+                    fixed_link = '|' + broken_link[1:-1].replace('|', '\|') + '|'
+                    text = text.replace(broken_link, fixed_link)
+                    changed = True
+
+                if changed:
                     file.seek(0)
                     file.write(text)
-
-
